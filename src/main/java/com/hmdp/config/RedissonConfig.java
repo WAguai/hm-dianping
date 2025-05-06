d618ed05780ee808abc061d4fe0a8737a32461b4
xuchong <1620987342@qq.com>
2025-05-06 20:58:31 +0800

    完成附近商户功能

--- src/main/java/com/hmdp/config/RedissonConfig.java
+++ src/main/java/com/hmdp/config/RedissonConfig.java
@@ -7,12 +7,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class RedisConfig {
+public class RedissonConfig {
     @Bean
     public RedissonClient redissonClient() {
         // 配置类
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://localhost:6380").setPassword("123456").setDatabase(1);
+        config.useSingleServer().setAddress("redis://localhost:6381").setPassword("123456").setDatabase(1);
         return Redisson.create(config);
     }
 }
